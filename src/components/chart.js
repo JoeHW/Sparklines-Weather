e4a288382dcf1416cdaf81bbd77f5e8d61fd7912
Joe Williamson <jhwilliamson@outlook.com>
2017-09-25 12:27:21 -0400

    humidity and pressure

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+ let average = (data) => {
+    return Math.floor((data.reduce((a,e) => a + e))/data.length)
+}
 
 export default (props) => {
     return (
         <div>
-            <Sparklines height={120} width={180} data={props.data}>
+            <Sparklines height={120} width={160} data={props.data}>
             <SparklinesLine color={props.color} />
+            <SparklinesReferenceLine type="avg" />    
             </Sparklines>
+            <div>
+            {average(props.data)} {props.units}
+            </div>
         </div>
     )
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,19 +7,20 @@ class WeatherList extends Component {
 renderWeather(cityData) {
  const name = cityData.city.name
  const temps = cityData.list.map(line => line.main.temp);
- 
+ const pressures = cityData.list.map(line => line.main.pressure);
+ const humidities = cityData.list.map(line => line.main.humidity);
  
   console.log(temps)  
     return (
      <tr key={name}>
         <td>{name}</td>
-        <td>
-            <Chart color={"green"} data={temps} />
-            </td>    
-        </tr>
-    )
-    
-}
+        <td><Chart color={"red"} data={temps} units="K"/></td>
+        <td><Chart color={"black"} data={pressures} units="hPa"/></td>
+        <td><Chart color={"blue"} data={humidities} units="%"/></td>
+        
+    </tr>
+  )
+ }
     
     render() {
         return (
@@ -27,9 +28,9 @@ renderWeather(cityData) {
             <thead>
              <tr>
               <th>City</th>
-              <th>Temperature</th>
-              <th>Pressure</th>
-              <th>Humidity</th>
+              <th>Temperature (K)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
                 </tr>
                 </thead>    
             <tbody>
